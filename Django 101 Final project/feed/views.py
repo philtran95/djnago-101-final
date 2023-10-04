920384de78b44898bb5b31278169c92bf3fe5839
philtran95 <132099050+philtran95@users.noreply.github.com>
2023-10-03 19:33:20 -0700

    Add files via upload

---
+++ Django 101 Final project/feed/views.py
@@ -0,0 +1,5 @@
+# from django.shortcuts import render * What does this even do
+# wtf does this next line even do.... 
+
+from django.contrib import messages 
+from django.views.generic import TemplateView, DetailView, FormView
